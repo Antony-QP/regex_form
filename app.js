c0f560e93a149f1ebe0715f93eec4a9a31fc4f01
Antony Parker <antonyqparker@gmail.com>
2020-09-03 13:35:03 +0100

    Complete form validation

--- app.js
+++ app.js
@@ -26,6 +26,24 @@ function validateZip() {
   }
 }
 
-function validateEmail() {}
+function validateEmail() {
+  const email = document.getElementById('email');
+  const re = /^([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5})$/
 
-function validatePhone() {}
+  if (!re.test(email.value)) {
+    email.classList.add('is-invalid');
+  } else {
+    email.classList.remove('is-invalid');
+  }
+}
+
+function validatePhone() {
+  const phone = document.getElementById('phone');
+  const re = /^\(?\d{3}\)?[-. ]?\d{3}[-. ]?\d{4}$/
+
+  if (!re.test(phone.value)) {
+    phone.classList.add('is-invalid');
+  } else {
+    phone.classList.remove('is-invalid');
+  }
+}
